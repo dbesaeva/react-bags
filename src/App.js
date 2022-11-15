7d6982ea09abdf4de841faa475b2292d5d2704bf
dbesaeva <daliyabesaeva1@gmail.com>
2022-11-15 20:29:23 +0700

    get data from server

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
-const arr = [
-  {title: 'Сумка Shoulder Pouch', price: 6990, imageUrl: "/img/bags/1.jpg"},
-  {title: 'Сумка Moonbag small', price: 10999, imageUrl: "/img/bags/2.jpg"},
-  {title: 'Сумка Pocket Bag mini', price: 12800, imageUrl: "/img/bags/3.jpg"},
-];
-
 function App() {
+  const [items, setItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('https://63737c01348e9472990db5c5.mockapi.io/items')
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      setItems(json);
+    });
+  }, []);
 
   return( 
   <div className="wrapper clear">
@@ -25,8 +30,8 @@ function App() {
         </div>
       </div>
 
-      <div className="d-flex"> 
-        {arr.map((obj) => (
+      <div className="d-flex flex-wrap"> 
+        {items.map((obj) => (
           <Card 
             title={obj.title} 
             price={obj.price} 
