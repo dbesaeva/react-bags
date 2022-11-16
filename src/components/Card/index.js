9671c0b5841b886998eac3605b05b97fa43daa2e
dbesaeva <daliyabesaeva1@gmail.com>
2022-11-16 13:42:41 +0700

    add items into cart

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import styles from './Card.module.scss';
 
-function Card(props) {
+function Card({onFavorite, title, price, imageUrl, onPlus}) {
     const [isAdded, setIsAdded] = React.useState(false);
 
     const onClickPlus = () => {
+        onPlus({title, price, imageUrl});
         setIsAdded(!isAdded);
     };
 
     return (
         <div className={styles.card}>
-            <div className={styles.favorite} onClick={props.onFavorite}>
+            <div className={styles.favorite} onClick={onFavorite}>
             <img src="/img/heart.svg" alt="Unliked"/>
             </div>
-            <img className={styles.image} width={129} height={198} src={props.imageUrl} alt="Bags"/>
-            <h5>{props.title}</h5>
+            <img className={styles.image} width={129} height={198} src={imageUrl} alt="Bags"/>
+            <h5>{title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                 <span>Цена:</span>
-                <b>{props.price} руб.</b>
+                <b>{price} руб.</b>
                 </div>
                 <img className={styles.plus} onClick={onClickPlus} src={isAdded ? "/img/btn-cheked.svg" : "/img/btn-plus.svg"} alt="Plus"/>
             </div>
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,28 +1,23 @@
-function Drawer(props) {
+function Drawer({ onClose, items = [] }) {
     return (
         <div className="overlay">
             <div className="drawer">
                 <h2 className="d-flex justify-between mb-30">Корзина 
-                <img onClick={props.onClose} className="cu-p" src="/img/btn-remove.svg" alt="Close"/>
+                <img onClick={onClose} className="cu-p" src="/img/btn-remove.svg" alt="Close"/>
                 </h2>
 
                 <div className="items"> 
-                <div className="cartItem d-flex align-center mb-20">
-                <div style={{ backgroundImage: 'url(/img/bags/1.jpg)'}} className="cartItemImg"></div>
-                <div className="mr-20 flex">
-                    <p className="mb-5">Сумка Shoulder Pouch</p>
-                    <b>6 990 руб.</b>
-                </div>
-                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
-                </div>
-                <div className="cartItem d-flex align-center mb-20">
-                <div style={{ backgroundImage: 'url(/img/bags/2.jpg)'}} className="cartItemImg"></div>
-                <div className="mr-20 flex">
-                    <p className="mb-5">Сумка Super Amini Bella</p>
-                    <b>10 999 руб.</b>
-                </div>
-                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
-                </div>
+                    {items.map((obj) => (
+                        <div className="cartItem d-flex align-center mb-20">
+                            <div style={{ backgroundImage: `url(${obj.imageUrl})` }} className="cartItemImg"></div>
+                            <div className="mr-20 flex">
+                                <p className="mb-5">{obj.title}</p>
+                                <b>{obj.price} руб.</b>
+                            </div>
+                            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
+                        </div>
+                    ))}
+
                 </div>
                 <div className="cartTotalBlock">
                 <ul>
